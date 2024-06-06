e86cf6807127b89d756ff46ba687efb9cbee2315
taiyangluoshanle <602461842@qq.com>
2024-06-06 22:58:15 +0800

    👔 return index.html

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { createExpressServer } from 'routing-controllers'
 import { json, urlencoded } from 'body-parser'
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import express, { Request, Response } from 'express'
+import path from 'node:path'
 
 import Routes from './routes'
 import logger from './utils/logger'
@@ -15,6 +17,13 @@ mongoose.connect(process.env.MONGODB_URI ?? '')
 const app = createExpressServer({
   controllers: [],
 })
+// 静态文件中间件
+app.use(express.static('dist'));
+
+// 处理所有路由请求并返回 index.html
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 app.use(json())
 app.use(urlencoded({ extended: true }))
 initMiddleware(app)
